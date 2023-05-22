1c2ebccb15302ee0689c4529f19a0c73cfab9135
Карпенко Дмитрий Дмитриевич <pr11karpenko@oat.local>
2023-05-22 17:37:03 +0600

    SeventhDay

--- app/src/main/java/com/example/myapplication/kod.java
+++ app/src/main/java/com/example/myapplication/kod.java
@@ -2,11 +2,16 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 public class kod extends AppCompatActivity {
 
@@ -22,6 +27,24 @@ public class kod extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        //Initializing thetextView
+        TextView textView;
+        textView = findViewById(R.id.hint);
 
+        new CountDownTimer(60000, 1000)
+        {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                //Used for formatting digit to be in 2 digits only
+                NumberFormat f = new DecimalFormat("00");
+
+                long sec = (millisUntilFinished/ 1000) % 60;
+                textView.setText("Отправить код повторно можно будет через " + f.format(sec) + " секунд");
+            }
+            //When the ask is over it will print 00:00:00 there
+            public void onFinish() {
+                textView.setText("Отправить код повторно можно будет через 0 секунд");
+            }
+        }.start();
     }
 }
--- app/src/main/java/com/example/myapplication/vhod.java
+++ app/src/main/java/com/example/myapplication/vhod.java
@@ -5,24 +5,57 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class vhod extends AppCompatActivity {
+    String email = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vhod);
-        Button next = findViewById(R.id.button);
-        next.setOnClickListener(new View.OnClickListener() {
+        Button next = findViewById(R.id.button213);
+        EditText emailValidate = findViewById(R.id.editText12);
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+        //String email = emailValidate.getText().toString().trim();
+        next.setEnabled(false);
+        emailValidate.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(vhod.this, kod.class);
-                startActivity(intent);
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                next.setEnabled(false);
+               next.setBackgroundResource(R.drawable.next_false);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                next.setEnabled(false);
+              next.setBackgroundResource(R.drawable.next_false);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                email = emailValidate.getText().toString().trim();
+                if (email.matches(emailPattern) && s.length() > 0) {
+                    next.setEnabled(true);
+                   next.setBackgroundResource(R.drawable.button_main);
+                    next.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intent = new Intent(vhod.this, kod.class);
+                            startActivity(intent);
+                        }
+                    });
+                } else {
+                    next.setEnabled(false);
+                   next.setBackgroundResource(R.drawable.next_false);
+
+                }
             }
         });
-
     }
 }
